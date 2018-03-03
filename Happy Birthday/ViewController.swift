b55cd721a4de24941c0cc62188b707d11bd09dff
Heather Mason <heathermason@Heathers-MacBook-Pro.local>
2018-03-03 15:57:22 -0800

    Initial Commit

---
+++ Happy Birthday/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happy Birthday
+//
+//  Created by Heather Mason on 3/3/18.
+//  Copyright © 2018 Heather Mason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
