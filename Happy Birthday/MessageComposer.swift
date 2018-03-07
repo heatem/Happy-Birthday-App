692d624058ae1a6e64cc3c1f913a8c62827da415
Heather Mason <heathermason@Heathers-MacBook-Pro.local>
2018-03-06 23:12:42 -0800

    Added messaging functionality and custom colors

---
+++ Happy Birthday/MessageComposer.swift
@@ -0,0 +1,13 @@
+//
+//  MessageComposer.swift
+//  Happy Birthday
+//
+//  Created by Heather Mason on 3/6/18.
+//  Copyright © 2018 Heather Mason. All rights reserved.
+//
+
+import UIKit
+
+class MessageComposer: NSObject {
+
+}
---
+++ Happy Birthday/UIColor.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor.swift
+//  Happy Birthday
+//
+//  Created by Heather Mason on 3/6/18.
+//  Copyright © 2018 Heather Mason. All rights reserved.
+//
+
+import Foundation
